9643d418cb1c52934556bb491444690301432a38
David Conner <robotics@cnu.edu>
2026-04-29 22:23:38 -0400

    Rebase from 'debian/kilted/ros2_snapshot'

---
+++ tests/test_node_specification_roundtrip.py
@@ -0,0 +1,45 @@
+# Copyright 2026 Christopher Newport University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ros2_snapshot.core.ros_model import BankType, ROSModel
+from ros2_snapshot.core.specifications.node_specification import NodeSpecificationBank
+from ros2_snapshot.core.specifications.package_specification import (
+    PackageSpecificationBank,
+)
+from ros2_snapshot.core.specifications.type_specification import TypeSpecificationBank
+
+
+def test_node_specification_json_roundtrip_preserves_multiple_file_paths(tmp_path):
+    model = ROSModel(
+        {
+            BankType.PACKAGE_SPECIFICATION: PackageSpecificationBank(),
+            BankType.NODE_SPECIFICATION: NodeSpecificationBank(),
+            BankType.MESSAGE_SPECIFICATION: TypeSpecificationBank(),
+            BankType.SERVICE_SPECIFICATION: TypeSpecificationBank(),
+            BankType.ACTION_SPECIFICATION: TypeSpecificationBank(),
+        }
+    )
+
+    model.node_specification_bank["demo_pkg/demo_node"].update_attributes(
+        package="demo_pkg",
+        file_path=["/real/demo_node", "/link/demo_node"],
+    )
+
+    model.save_model_json_files(tmp_path, "snapshot")
+    loaded_model = ROSModel.read_model_from_json(tmp_path, "snapshot", spec_only=True)
+
+    assert loaded_model.node_specification_bank["demo_pkg/demo_node"].file_path == [
+        "/real/demo_node",
+        "/link/demo_node",
+    ]
